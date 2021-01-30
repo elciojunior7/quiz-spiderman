7ff45ea3d71a5c4c3c718a1e21f98de4d6074d7e
Elcio Junior <elciosouza.junior@gmail.com>
2021-01-30 16:07:33 -0300

    og-v2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,16 +11,17 @@ function Header() {
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://quiz-spiderman.vercel.app/" />
       <meta property="og:title" content="Quiz do Aranha" />
-      <meta property="og:image" content="https://raw.githubusercontent.com/elciojunior7/quiz-spiderman/main/public/assets/bg.jpg" />
+      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://quiz-spiderman.vercel.app/" />
       <meta property="twitter:title" content="Quiz do Aranha" />
-      <meta property="twitter:image" content="https://raw.githubusercontent.com/elciojunior7/quiz-spiderman/main/public/assets/bg.jpg" />
+      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
 
-      <link rel="shortcut icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+
+      <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
   );
 }
